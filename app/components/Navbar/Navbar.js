2b6d22221d42e200c07b0f5b93cc66cad340cf6f
Raahim <raahimaligcu@gmail.com>
2023-12-13 17:28:20 +0500

    responsive navbar made

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,42 +1,76 @@
 "use client";
 import "./Navbar.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Greenbtn from "../Greenbtn";
 function Navbar() {
   const router = useRouter();
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
+    console.log("hi");
+    setDropdownOpen((prev) => !prev);
+  };
+
   return (
-    <div className="navbar">
-      <div
-        className="logo"
-        onClick={() => {
-          router.push("/Home");
-        }}
-      >
-        OCTALCODE
-      </div>
-      <div className="navbar-items">
+    <>
+      <div className="navbar">
         <div
-          className="navbarIcon"
+          className="logo"
           onClick={() => {
-            router.push("/");
+            router.push("/Home");
           }}
         >
-          HOME
+          OCTALCODE
         </div>
-        <div
-          className="navbarIcon"
-          onClick={() => {
-            router.push("/work");
-          }}
-        >
-          WHY US
+        <div className="navbar-items">
+          <div
+            className="navbarIcon"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            HOME
+          </div>
+          <div
+            className="navbarIcon"
+            onClick={() => {
+              router.push("/work");
+            }}
+          >
+            WHY US
+          </div>
+          <div className="navbarIcon">CASE STUDIES</div>
+          <div className="navbarIcon">FAQs</div>
+          <Greenbtn buttonText="Contact us" href="/Contact" />
         </div>
-        <div className="navbarIcon">CASE STUDIES</div>
-        <div className="navbarIcon">FAQs</div>
-        <Greenbtn buttonText="Contact us" href="/Contact" />
+        <button className="sidebarBtn" onClick={toggleDropdown}>
+          ☰
+        </button>
       </div>
-      <button className="sidebarBtn">☰</button>
-    </div>
+      {isDropdownOpen ? (
+        <div className="dropdownMenu">
+          <div
+            className="navbarIcon"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            HOME
+          </div>
+          <div
+            className="navbarIcon"
+            onClick={() => {
+              router.push("/work");
+            }}
+          >
+            WHY US
+          </div>
+          <div className="navbarIcon">CASE STUDIES</div>
+          <div className="navbarIcon">FAQs</div>
+          <Greenbtn buttonText="Contact us" href="/Contact" />
+        </div>
+      ) : null}
+    </>
   );
 }
 
