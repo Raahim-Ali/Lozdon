dd10ea749f3ff00f293c0a75327f37bd5687eeca
Raahim <raahimaligcu@gmail.com>
2023-12-14 18:44:31 +0500

    swiper done

--- app/work/Components/Slider.js
+++ app/work/Components/Slider.js
@@ -1,259 +1,94 @@
 "use client";
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+// Import necessary modules
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ClientOne from "../../../public/Assets/Work/ClientOne.svg";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
-import "./Slider.css";
-
-// import required modules
 import { Pagination } from "swiper/modules";
 import Image from "next/image";
+import "./Slider.css";
 
-export default function Slider() {
+const sliderData = [
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+  {
+    title: "The best Digital Agency",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
+    name: "Sophia Moore1",
+  },
+];
+
+const Slider = () => {
   return (
-    <>
-      <div className="SwiperContainer">
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
+    <div className="SwiperContainer">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {sliderData.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div style={{ marginBottom: "20px" }}>
+              <p className="swiperTitle">{slide.title}</p>
             </div>
             <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
+              <p className="SwiperText">{slide.text}</p>
             </div>
             <div className="SwiperClient">
               <div>
                 <Image src={ClientOne} alt="/" width={100} height={100} />
               </div>
               <div>
-                <p className="SwiperClientName">Sophia Moore1</p>
+                <p className="SwiperClientName">{slide.name}</p>
                 <p className="SwiperClientDetail">CEO at Webflow Agency</p>
               </div>
             </div>
           </SwiperSlide>
-
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore2</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore3</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore4</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore5</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore6</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore7</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore8</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              style={{
-                marginBottom: "20px",
-              }}
-            >
-              <p className="swiperTitle">“The best Digital Agency”</p>
-            </div>
-            <div style={{ marginBottom: "29px" }}>
-              <p className="SwiperText">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat aute irure sint
-                amet occaecat cupidatat non proident
-              </p>
-            </div>
-            <div className="SwiperClient">
-              <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
-              </div>
-              <div>
-                <p className="SwiperClientName">Sophia Moore9</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </>
+        ))}
+      </Swiper>
+    </div>
   );
-}
+};
+
+export default Slider;
