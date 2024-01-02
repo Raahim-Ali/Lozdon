9bd78fcebae4a2d72e8dfa078de935c962a64261
Raahim <raahimaligcu@gmail.com>
2024-01-02 19:32:03 +0500

    ..

--- app/Services/App/page.js
+++ app/Services/App/page.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ContentAfterHeroSection from "./ContentAfterHeroSection";
 import HeroSection from "../../components/HeroSection";
-import ProcessCards from "@/app/Home/ProcessCards";
 
 export default function page() {
   return (
     <>
       <HeroSection
-        title="Mobile Devlopment"
+        title="App Devlopment"
         paragraph="It encompasses design, coding, testing and optimization to deliver user-friendly and efficient mobile applications across iOS and Android platforms."
         btnText="let’s work together"
         btnWidth="28%"
         imgSrc="/Assets/Herosection/HeroSectionImage.svg"
       />
-      <ProcessCards />
       <ContentAfterHeroSection />
     </>
   );
