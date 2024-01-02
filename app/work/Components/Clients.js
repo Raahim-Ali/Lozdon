939548680e8016455537e5d363b64ff41acb84e8
Raahim <raahimaligcu@gmail.com>
2024-01-02 14:29:45 +0500

    button transition added

--- app/work/Components/Clients.js
+++ app/work/Components/Clients.js
@@ -1,48 +1,14 @@
 "use client";
-import Image from "next/image";
 import Slider from "../Components/Slider";
-import AirTable from "../../../public/Assets/Work/AirTable.svg";
-import ClientOne from "../../../public/Assets/Work/ClientOne.svg";
 import "./Clients.css";
 function Clients() {
   return (
-    <div className="clientmain">
-      <div className="clientheading">
-        <p className="clientheadingText">WHAT OUR CLIENTS SAY</p>
+    <>
+      <div className="clientmain">
+        <p className="IndustriesHeading">WHAT OUR CLIENTS SAY</p>
+        <Slider />
       </div>
-      <Slider />
-      <div className="videoContainer">
-        <iframe
-          className="video"
-          height="315"
-          src="https://www.youtube.com/embed/9QgbaU29lIw?si=quCHlLtH21JoKCXz"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-
-        <div className="videoContent">
-          <div>
-            <Image src={AirTable} alt="/" />
-          </div>
-          <div>
-            <p className="videotext">
-              &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua ut
-              enim ad minim veniam, quis nostrud&quot;
-            </p>
-          </div>
-          <div className="clientDetail">
-            <Image src={ClientOne} alt="/" />
-            <div>
-              <p className="clientName">Soophia Moore</p>
-              <p className="clientInfo">CEO at Webflow Agency</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
--- app/work/Components/Slider.js
+++ app/work/Components/Slider.js
@@ -2,70 +2,78 @@
 // Import necessary modules
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import ClientOne from "../../../public/Assets/Work/ClientOne.svg";
+import ClientOne from "../../../public/Assets/Work/google.png";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
+
 import Image from "next/image";
 import "./Slider.css";
 
 const sliderData = [
   {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
+    title: "Outstanding Service",
+    text: "I recently had a dental checkup at Dental Essence and I'm impressed. The staff is friendly, and the clinic is clean and inviting. The dentists were thorough, explaining each step with patience. I highly recommend Dental Essence for top-notch dental care.",
+    name: "Hammad Abid",
+  },
+
+  {
+    title: " Caring Professionals",
+    text: "My experience at Dental Essence was excellent. The dentists and staff are compassionate and attentive. They made me feel at ease throughout my procedure.",
+    name: "Saad Rana",
   },
   {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
+    title: "Modern Clinic, Expert Care",
+    text: "I'm impressed with the modern facilities and expert care. The dental team is knowledgeable, and the clinic uses cutting-edge technology. I felt well-informed about my treatment options, and the staff made my visit comfortable.",
+    name: "Ayesha",
   },
   {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
+    title: "Quick and Convenient Dental Service",
+    text: "I had a convenient experience. The appointment started on time. The clinic's efficiency and friendliness make it a go-to choice for those with a busy schedule. I'm pleased with the service and will continue to choose Dental Essence for my dental needs.",
+    name: "Muhammad Hasnain",
   },
   {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
+    title: "Top-Notch Cosmetic Dentistry",
+    text: "I recently had cosmetic dental work done, and the results exceeded my expectations. The attention to detail and commitment to achieving the desired outcome were impressive. The cosmetic dentists are true professionals, and the staff provided exceptional service.",
+    name: "Ahmad Kaleem",
   },
   {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
-  },
-  {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
-  },
-  {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
-  },
-  {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
-  },
-  {
-    title: "The best Digital Agency",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident",
-    name: "Sophia Moore1",
+    title: "Emergency Care Excellence",
+    text: "I had an unexpected dental emergency and needed immediate assistance. The team at Dental Essence demonstrated outstanding responsiveness and professionalism. The dentist addressed my issue promptly, and the support staff was reassuring.",
+    name: "Rida Fatima",
   },
 ];
 
 const Slider = () => {
   return (
-    <div className="SwiperContainer">
+    <div className="swiper-container">
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
-        pagination={{ clickable: true }}
-        modules={[Pagination]}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
+        breakpoints={{
+          1500: {
+            slidesPerView: 3,
+          },
+          1200: {
+            slidesPerView: 2,
+          },
+          1000: {
+            slidesPerView: 1,
+          },
+          100: {
+            slidesPerView: 1,
+          },
+        }}
       >
         {sliderData.map((slide, index) => (
           <SwiperSlide key={index}>
@@ -77,16 +85,27 @@ const Slider = () => {
             </div>
             <div className="SwiperClient">
               <div>
-                <Image src={ClientOne} alt="/" width={100} height={100} />
+                <Image src={ClientOne} alt="/" width={40} height={40} />
               </div>
               <div>
                 <p className="SwiperClientName">{slide.name}</p>
-                <p className="SwiperClientDetail">CEO at Webflow Agency</p>
               </div>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="swiper-button-next">
+        <img
+          src="/Assets/Work/right.svg"
+          style={{ transform: "rotate(90deg)" }}
+        />
+      </div>
+      <div className="swiper-button-prev">
+        <img
+          src="/Assets/Work/right.svg"
+          style={{ transform: "rotate(-90deg)" }}
+        />
+      </div>
     </div>
   );
 };
