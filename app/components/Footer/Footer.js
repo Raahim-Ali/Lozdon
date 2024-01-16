e1c53181867a66470a1e1afeb0f4d0665a8ea1cf
Raahim <raahimaligcu@gmail.com>
2024-01-16 17:55:17 +0500

    ..

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -109,7 +109,7 @@ export default function Footer() {
                 <img src="/Assets/Footer/Call.svg" />
                 <Link href="tel:0342-32020441">
                   <p className="inter text-white text-base font-normal">
-                    +92 42 32020441
+                    +92 321 4349743
                   </p>
                 </Link>
               </div>
