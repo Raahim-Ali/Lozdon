85da79cfa75ebcfbc468bbaceb98e5e9fb3d3eea
Raahim <raahimaligcu@gmail.com>
2024-02-15 15:41:31 +0500

    made icons in assets

--- app/Services/App/ContentAfterHeroSection.js
+++ app/Services/App/ContentAfterHeroSection.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ImageAndDiv from "./container/ImageAndDiv";
 import Idea from "../../Home/Idea";
-import LatestTechnologies from "./container/LatestTechnologies";
 
 export default function ContentAfterHeroSection() {
   return (
     <>
       <div>
-        <LatestTechnologies />
         <ImageAndDiv />
         <Idea />
       </div>
