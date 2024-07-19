55de055d0cf8f29286302b9f537d97f32e05e2a6
Raahim <raahimaligcu@gmail.com>
2024-07-19 18:48:20 +0500

    about us handled

--- app/Home/HeroSection.js
+++ app/Home/HeroSection.js
@@ -1,63 +1,40 @@
 "use client";
-import React, { useState } from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React from "react";
 
-const HeroSection = ({ slides, title, paragraph, imgSrc }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handleSlideChange = (index) => {
-    setCurrentSlide(index);
-  };
-
+const HeroSection = ({ title, paragraph, imgSrc }) => {
   return (
-    <Carousel
-      selectedItem={currentSlide}
-      onChange={handleSlideChange}
-      showThumbs={false}
-      showArrows={false}
-      showStatus={false}
-      infiniteLoop={true}
-      autoPlay={true}
-      interval={200000}
-      stopOnHover={false}
+    <div
+      className="bg-cover bg-center flex items-center justify-center shadow-md"
+      style={{
+        backgroundImage: `linear-gradient(256.89deg, rgba(9, 14, 142, 0) -17.66%, rgba(9, 14, 142, 0.9) 90%), url(${imgSrc})`,
+      }}
     >
-      {slides.map((slide, index) => (
-        <div
-          key={index}
-          className="  bg-cover bg-center flex items-center justify-center shadow-md"
+      <div className="text-white px-4 sm:px-8 max-w-full flex flex-col gap-[30px] py-[180px] items-start text-left">
+        <ul className="flex gap-8 list-disc items-start md:text-base sm:text-xs text-[0.7rem] justify-start">
+          <li className="marker:font-normal list-none uppercase">
+            Professionalism
+          </li>
+          <li className="marker:font-normal uppercase">Excellence</li>
+          <li className="marker:font-normal uppercase">Timeliness</li>
+        </ul>
+        <h1
+          className="uppercase text-3xl sm:text-4xl font-bold md:text-[4rem] md:leading-[1.3]  text-left md:w-[70vw]"
           style={{
-            backgroundImage: `linear-gradient(256.89deg, rgba(9, 14, 142, 0) -17.66%, rgba(9, 14, 142, 0.9) 90%), url(${imgSrc})`,
+            fontFamily: "Libre Caslon Text",
           }}
         >
-          <div className="text-white px-4 sm:px-8 max-w-full flex flex-col gap-[30px] py-[180px] items-start text-left">
-            <ul className="flex gap-8 list-disc items-start md:text-base sm:text-xs text-[0.7rem] justify-start">
-              <li className="marker:font-normal list-none uppercase">
-                Professionalism
-              </li>
-              <li className="marker:font-normal uppercase">Excellence</li>
-              <li className="marker:font-normal uppercase">Timeliness</li>
-            </ul>
-            <h1
-              className="uppercase text-3xl sm:text-4xl font-bold md:text-[4rem] md:leading-[1.3]  text-left md:w-[70vw]"
-              style={{
-                fontFamily: "Libre Caslon Text",
-              }}
-            >
-              {title}
-            </h1>
-            <p
-              className="sm:text-whitePrimary text-restext sm:w-full md:text-base sm:text-xs pt-[20px]  font-light  leading-[2rem] md:w-[45vw]"
-              style={{
-                fontFamily: "Inter",
-              }}
-            >
-              {paragraph}
-            </p>
-          </div>
-        </div>
-      ))}
-    </Carousel>
+          {title}
+        </h1>
+        <p
+          className="sm:text-whitePrimary text-restext sm:w-full md:text-base sm:text-xs pt-[20px]  font-light  leading-[2rem] md:w-[45vw]"
+          style={{
+            fontFamily: "Inter",
+          }}
+        >
+          {paragraph}
+        </p>
+      </div>
+    </div>
   );
 };
 
