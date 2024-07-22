7a5a920a4ee5dd5dd0da72c76cbc1441791de4d4
Raahim <raahimaligcu@gmail.com>
2024-07-22 20:04:40 +0500

    changes made

--- app/Products/HeroSection.js
+++ app/Products/HeroSection.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./HeroSection.css";
-import "../Home/About.css";
 
 function HeroSection({ heading, subHeading, text, imageSrc }) {
   return (
     <div>
       <div className="heroSection">
         <div className="textSection" style={{ paddingTop: "30px" }}>
-          <div className="Heading">
-            <p>{heading}</p>
+          <div>
+            <p className="HeroHeading">{heading}</p>
           </div>
-          <div className="Heading2" style={{ paddingBottom: "20px" }}>
-            <p style={{ fontSize: "48px", lineHeight: "60px" }}>{subHeading}</p>
+          <div style={{ paddingBottom: "20px" }}>
+            <p className="HeroHeading2" style={{ lineHeight: "60px" }}>
+              {subHeading}
+            </p>
           </div>
           <div className="text1">
             <p style={{ fontSize: "18px", lineHeight: "30px" }}>{text}</p>
